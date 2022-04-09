bcfbd669bd5f12495c223c66d5132510faa644bf
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-04-09 11:07:06 +0300

    Initial commit

---
+++ Formula1/Formula1/Core/Controller.cs
@@ -0,0 +1,187 @@
+﻿using Formula1.Core.Contracts;
+using Formula1.Models;
+using Formula1.Models.Contracts;
+using Formula1.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Formula1.Core
+{
+    public class Controller : IController
+    {
+        private PilotRepository pilotRepository;
+        private RaceRepository raceRepository;
+        private FormulaOneCarRepository carRepository;
+
+        public Controller()
+        {
+            pilotRepository = new PilotRepository();
+            raceRepository = new RaceRepository();
+            carRepository = new FormulaOneCarRepository();
+        }
+
+        public string AddCarToPilot(string pilotName, string carModel)
+        {
+            var car = carRepository.FindByName(carModel);
+            var pilot = pilotRepository.FindByName(pilotName);
+
+            if (pilot == null || pilot.Car != null)
+            {
+                throw new InvalidOperationException($"Pilot {pilotName} does not exist or has a car.");
+            }
+            if (car == null)
+            {
+                throw new NullReferenceException($"Car {carModel} does not exist.");
+            }
+
+            pilot.AddCar(car);
+            carRepository.Remove(car);
+
+            return $"Pilot {pilotName} will drive a {car.GetType().Name} {carModel} car.";
+        }
+
+        public string AddPilotToRace(string raceName, string pilotFullName)
+        {
+            var pilot = pilotRepository.FindByName(pilotFullName);
+            var race = raceRepository.FindByName(raceName);
+
+            if (race == null)
+            {
+                throw new NullReferenceException($"Race {raceName} does not exist.");
+            }
+
+            var isPilotInRace = race.Pilots.Contains(pilot);
+
+            // TODO :
+            if (pilot == null || !pilot.CanRace || isPilotInRace)
+            {
+                throw new InvalidOperationException($"Can not add pilot {pilotFullName} to the race.");
+            }
+
+            race.AddPilot(pilot);
+
+            return $"Pilot {pilotFullName} is added to the {raceName} race.";
+        }
+
+        public string CreateCar(string type, string model, int horsepower, double engineDisplacement)
+        {
+            var checkIfExist = carRepository.FindByName(model);
+
+            if (checkIfExist != null)
+            {
+                throw new InvalidOperationException($"Formula one car {model} is already created.");
+            }
+
+            IFormulaOneCar car = null;
+
+            if (type == "Ferrari")
+            {
+                car = new Ferrari(model, horsepower, engineDisplacement);
+            }
+            else if (type == "Williams")
+            {
+                car = new Williams(model, horsepower, engineDisplacement);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Formula one car type {type} is not valid.");
+            }
+
+            carRepository.Add(car);
+
+            return $"Car {type}, model {model} is created.";
+        }
+
+        public string CreatePilot(string fullName)
+        {
+            var pilot = pilotRepository.FindByName(fullName);
+
+            if (pilot != null)
+            {
+                throw new InvalidOperationException($"Pilot {fullName} is already created.");
+            }
+
+            var newPilot = new Pilot(fullName);
+
+            pilotRepository.Add(newPilot);
+
+            return $"Pilot {fullName} is created.";
+        }
+
+        public string CreateRace(string raceName, int numberOfLaps)
+        {
+            var race = raceRepository.FindByName(raceName);
+
+            if (race != null)
+            {
+                throw new InvalidOperationException($"Race {raceName} is already created.");
+            }
+
+            var newRace = new Race(raceName, numberOfLaps);
+
+            raceRepository.Add(newRace);
+
+            return $"Race {raceName} is created.";
+        }
+
+        public string PilotReport()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var pilot in pilotRepository.Models.OrderByDescending(x => x.NumberOfWins))
+            {
+                sb.AppendLine(pilot.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RaceReport()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var race in raceRepository.Models.Where(x => x.TookPlace))
+            {
+                sb.AppendLine(race.RaceInfo());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string StartRace(string raceName)
+        {
+            var race = raceRepository.FindByName(raceName);
+
+            if (race == null)
+            {
+                throw new NullReferenceException($"Race {raceName} does not exist.");
+            }
+
+            if (race.Pilots.Count < 3)
+            {
+                throw new InvalidOperationException($"Race {raceName} cannot start with less than three participants.");
+            }
+
+            if (race.TookPlace)
+            {
+                throw new InvalidOperationException($"Can not execute race {raceName}.");
+            }
+
+            var riders = pilotRepository.Models.OrderByDescending(x => x.Car.RaceScoreCalculator(race.NumberOfLaps)).Take(3).ToList();
+
+            race.TookPlace = true;
+
+            riders[0].WinRace();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Pilot {riders[0].FullName} wins the {raceName} race.")
+                .AppendLine($"Pilot {riders[1].FullName} is second in the {raceName} race.")
+                .AppendLine($"Pilot {riders[2].FullName} is third in the {raceName} race.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Formula1/Formula1/Models/Race.cs
@@ -0,0 +1,69 @@
+﻿using Formula1.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+
+        public Race(string raceName, int numberOfLaps)
+        {
+            this.RaceName = raceName;
+            this.NumberOfLaps = numberOfLaps;
+            this.Pilots = new List<IPilot>();
+        }
+
+        public string RaceName
+        {
+            get => raceName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException($"Invalid race name: {value}.");
+                }
+                raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get => numberOfLaps;
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException($"Invalid lap numbers: {value}.");
+                }
+                numberOfLaps = value;
+            }
+        }
+
+        public bool TookPlace { get; set; }
+
+        public ICollection<IPilot> Pilots {get;}
+
+        public void AddPilot(IPilot pilot)
+        {
+            this.Pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var tookPlace = TookPlace == true ? "Yes" : "No";
+
+            sb.AppendLine($"The {this.RaceName} race has:")
+                .AppendLine($"Participants: {this.Pilots.Count}")
+                .AppendLine($"Number of laps: {this.NumberOfLaps}")
+                .AppendLine($"Took place: {tookPlace}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
